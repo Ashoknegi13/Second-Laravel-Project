deafb4d9cf865e9897ba1ade5c5abce5d053c4a8
Ashok <negiashok132@gmail.com>
2025-01-10 01:45:54 +0530

    in this case i just send a normal variable and single array dynamic data from route to view file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users',function(){
+     $name = " ashok Singh negi";
+    
+    return view('users',[
+                       'user'=>$name,
+                       'city'=>''
+                    ]);
+    
+    
+     // return view('users')
+    // ->withUser($name)
+    // ->withCity('Delhi');
+ 
+ 
+ 
+     // return view('users')
+    // ->with('user',$name)
+    // ->with('city','rudraprayag');
+
+ 
+  
+    // return view('users',['user'=>$name,
+    //                       'city'=>'<script>alert("Rudraprayag");</script>'
+    //                     ]);
+});   // this si users route brackets
